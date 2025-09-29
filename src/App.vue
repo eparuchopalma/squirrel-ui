0e559e4e7308e2d1d59e988c7d1d99fdcddb02bb
Edgar Parucho Palma <eparuchopalma@outlook.com>
2025-09-29 11:00:50 -0400

    Starts repository adding basic theme styles. - Set up fonts to be implemented for the UI. - Add variables with the brand colors. - Add Squirrel logo. - Remove framework's styles, assets.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1 class="font-headline">Welcome to Squirrel</h1>
+    <p>A financial log.</p>
+  </main>
+  <footer class="font-accent">
+    <p>Thank you for being here</p>
+    <br>
+    <p>Edgar</p>
+  </footer>
+</template>
+
+<style scoped>
+</style>
